8c99203b68390465e4463cb033c291a6f607c86b
4ly <alyssonalcantaralves@gmail.com>
2020-12-03 11:29:14 -0300

    Fix client

--- Client.js
+++ Client.js
@@ -3,14 +3,26 @@ const informacoesClientes = [
     cpf: 18875539081,
     nome: "Elena",
   },
+  {
+    cpf: 28875539081,
+    nome: "Agnaldo",
+  },
+  
 ];
-
-const conteudoLinha = 
-`<tr>
-  <td>${informacoesClientes[0].cpf}</td>
-  <td>${informacoesClientes[0].nome}</td>
-</tr>`;
 
 const corpoTabela = document.querySelector("[data-conteudo-tabela]");
 
-corpoTabela.innerHTML = conteudoLinha;
+const exibeCliente = (cpf, nome) => {
+  const linha = document.createElement("tr");
+  const conteudoLinha = `
+  <td>${cpf}</td>
+  <td>${nome}</td>
+  `;
+  linha.innerHTML = conteudoLinha;
+  return linha;
+  //sempre retorne :>
+};
+
+informacoesClientes.forEach((indice) => {
+  corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
+});
