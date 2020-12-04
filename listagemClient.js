7033b6af98d0019181660d58fbdf0931f7f09ea1
4ly <alyssonalcantaralves@gmail.com>
2020-12-04 09:11:21 -0300

    listagemClient

---
+++ listagemClient.js
@@ -0,0 +1,16 @@
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+const exibeCliente = (cpf, nome) => {
+  const linha = document.createElement("tr");
+  const conteudoLinha = `
+  <td>${cpf}</td>
+  <td>${nome}</td>
+  `;
+  linha.innerHTML = conteudoLinha;
+  return linha;
+  //sempre retorne :>
+};
+
+informacoesClientes.forEach((indice) => {
+  corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
+});
