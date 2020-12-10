50cb8b65ba5cdb9b886e4f1be4d2b5b76361aeb9
4ly <alyssonalcantaralves@gmail.com>
2020-12-10 10:44:52 -0300

    update client

--- api/client.js
+++ api/client.js
@@ -1,7 +1,31 @@
-fetch("http://localhost:4000/clientes")
-  .then((resposta) => {
-    return resposta.json();
-  })
-  .then((json) => {
-    return json;
+//get method
+
+const listarClientes = () => {
+  return fetch("http://localhost:4000/clientes")
+    .then((resposta) => {
+      return resposta.json();
+    })
+    .then((json) => {
+      return json;
+    });
+};
+
+const cadastrarClientes = (nome, cpf) => {
+  const Json = JSON.stringify({
+    nome: nome,
+    cpf: cpf,
   });
+
+  return fetch(
+    "http://localhost:4000/clientes/cliente",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: Json,
+    }.then((resp) => {
+      return resp.body;
+    })
+  );
+};
